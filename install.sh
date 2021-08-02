6d696c42b519ebbfb3659d38a2e8e605902311d7
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2021-08-02 15:51:31 -0400

    apparently I changed something

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ if [ ! -x "$(command -v docker)" ]; then
 	sh scripts/get-docker.sh
 fi
 
-# Copy docker run script to local bin
-cp scripts/nvimserv_run.sh /usr/local/bin/nvimserv_run.sh
-
-# Make executable
-chmod +x /usr/local/bin/nvimserv_run.sh
-
 # Add alias to .bashrc
 grep -qxF "alias nvim=/usr/local/bin/nvimserv_run.sh" ~/.bashrc || echo "alias nvim=/usr/local/bin/nvimserv_run.sh" >> ~/.bashrc
 source ~/.bashrc
+
+# Copy docker run script to local bin
+sudo cp scripts/nvimserv_run.sh /usr/local/bin/nvimserv_run.sh
+
+# Make executable
+sudo chmod +x /usr/local/bin/nvimserv_run.sh
+
